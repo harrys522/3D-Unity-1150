5ed9ab8c32c56d4fb2004bc2afd2138f84876350
harrys522 <harry.sackman@students.mq.edu.au>
2022-05-26 13:36:26 +1000

    Full semester progress

---
+++ Assets/Interactivity/ClickToAnim.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class ClickToAnim : MonoBehaviour
+{
+    public Animator animator;
+    public string lookAtTriggerName = "LookAt";
+    public string lookAwayTriggerName = "LookAway";
+    public string lookingAtBoolName = "LookingAt";
+
+    public string mouseDownTriggerName = "MouseDown";
+    public string mouseUpTriggerName = "MouseUp";
+    public string mouseDragBoolName = "MouseDrag";
+    private bool isMouseOver = false;
+    private bool isMouseDrag = false;
+
+    void Update()
+    {
+
+        RaycastHit hitInfo;
+
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        if (Physics.Raycast(ray, out hitInfo) && hitInfo.collider.gameObject == gameObject)
+        {
+            if (!isMouseOver)
+            {
+                animator.SetTrigger(lookAtTriggerName);
+                isMouseOver = true;
+            }
+
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                animator.SetTrigger(mouseDownTriggerName);
+                isMouseDrag = true;
+            }
+
+        }
+        else
+        {
+            if (isMouseOver)
+            {
+                animator.SetTrigger(lookAwayTriggerName);
+                isMouseOver = false;
+            }
+        }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame && isMouseDrag)
+        {
+            animator.SetTrigger(mouseUpTriggerName);
+            isMouseDrag = false;
+        }
+
+        animator.SetBool(lookingAtBoolName, isMouseOver);
+        animator.SetBool(mouseDragBoolName, isMouseDrag);
+
+    }
+
+}
---
+++ Assets/Interactivity/TriggerToAnim.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerToAnim : MonoBehaviour {
+
+	public Animator animator;
+	public LayerMask layerMask = -1;
+	public string enterTriggerName = "Enter";
+	public string exitTriggerName = "Exit";
+	public string occupiedBoolName = "Occupied";
+	private bool isOccupied = false;
+
+	public void FixedUpdate() {
+		animator.SetBool(occupiedBoolName, isOccupied);
+		isOccupied = false;
+	}
+
+	private bool InMask(Collider collider) {
+		return ((layerMask.value & (1 << collider.gameObject.layer)) > 0);
+	}
+
+	public void OnTriggerEnter(Collider collider) {
+		if (InMask(collider)) {
+			animator.SetTrigger(enterTriggerName);
+		}
+	}
+
+	public void OnTriggerExit(Collider collider) {
+		if (InMask(collider)) {
+			animator.SetTrigger(exitTriggerName);
+		}
+	}
+
+	public void OnTriggerStay(Collider collider) {
+		if (InMask(collider)) {
+			isOccupied = true;
+		}
+	}
+
+}
